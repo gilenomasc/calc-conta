fb323a9ee20271ebd7776a455732396985044520
Gileno Mascarenhas <gilenomasc@hotmail.com>
2021-11-27 14:07:11 -0300

    make it better

--- util/db.js
+++ util/db.js
@@ -1,20 +1,20 @@
 
 const mysql = require('mysql2');
-const db = mysql.createConnection({
+
+let db;
+
+const db_config = {
     host: 'us-cdbr-east-04.cleardb.com',
     user: 'b7c6317eab1cef',
     password: '796f7a47',
     port: 3306,
     database: 'heroku_7b2ad30fa60e245',
     multipleStatements: true
-});
+};
 
-db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Conectado ao MySQL');
-});
+   db = mysql.createPool(db_config);
+  
+console.log("Essa merda de merda");
 
 global.db = db;
 

6b8ba89f8fa86b0589c99b4833e6281db2665e6c
Gileno Mascarenhas <gilenomasc@hotmail.com>
2023-03-24 22:20:22 -0300

    Commit inicial

--- util/db.js
+++ util/db.js
@@ -1,20 +1,19 @@
-
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 let db;
 
 const db_config = {
-    host: 'us-cdbr-east-04.cleardb.com',
-    user: 'b7c6317eab1cef',
-    password: '796f7a47',
-    port: 3306,
-    database: 'heroku_7b2ad30fa60e245',
-    multipleStatements: true
+  host: "us-cdbr-east-04.cleardb.com",
+  user: "b7c6317eab1cef",
+  password: "796f7a47",
+  port: 3306,
+  database: "heroku_7b2ad30fa60e245",
+  multipleStatements: true,
 };
 
-   db = mysql.createPool(db_config);
-  
-console.log("Essa merda de merda");
+db = mysql.createPool(db_config);
+
+console.log("Database remoto ok!");
 
 global.db = db;
 
